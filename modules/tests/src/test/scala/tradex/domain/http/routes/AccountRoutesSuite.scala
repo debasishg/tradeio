b1c8ce7bb47d0bda536f0f8513781ff9a2bec959
Debasish Ghosh <ghosh.debasish@gmail.com>
2021-09-05 00:16:20 +0530

    Started writing generators and some tests for http

--- modules/tests/src/test/scala/tradex/domain/http/routes/AccountRoutesSuite.scala
+++ modules/tests/src/test/scala/tradex/domain/http/routes/AccountRoutesSuite.scala
@@ -2,11 +2,17 @@ package tradex.domain
 package http.routes
 
 import java.time.LocalDate
+import org.scalacheck.Gen
 import cats.effect._
 import cats.syntax.all._
+import org.http4s.Method._
+import org.http4s._
+import org.http4s.client.dsl.io._
+import org.http4s.syntax.literals._
 import suite.HttpSuite
 import repository.AccountRepository
 import model.account._
+import generators._
 
 object AccountRoutesSuite extends HttpSuite {
   def dataAccounts(accounts: List[Account]) = new TestAccountRepository {
@@ -40,6 +46,22 @@ object AccountRoutesSuite extends HttpSuite {
       IO.raiseError(DummyError) *> IO.pure(accounts.find(_.no === no))
     override def query(openedOn: LocalDate): IO[List[Account]]              = all
     override def allClosed(closeDate: Option[LocalDate]): IO[List[Account]] = all
+  }
+
+  test("GET accounts succeeds") {
+    forall(Gen.listOf(accountGen)) { accounts =>
+      val req    = GET(uri"/accounts")
+      val routes = AccountRoutes[IO](dataAccounts(accounts)).routes
+      expectHttpBodyAndStatus(routes, req)(accounts, Status.Ok)
+    }
+  }
+
+  test("GET accounts fails") {
+    forall(Gen.listOf(accountGen)) { accounts =>
+      val req    = GET(uri"/accounts")
+      val routes = AccountRoutes[IO](failingAccounts(accounts)).routes
+      expectHttpFailure(routes, req)
+    }
   }
 }
 
