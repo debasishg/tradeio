9c57d51f552cfcaebf797b11dbf95b3bbc7374c4
Debasish Ghosh <ghosh.debasish@gmail.com>
2021-10-17 19:06:17 +0530

    Added fail case for AdminAccountRoutesSuite

--- modules/tests/src/test/scala/tradex/domain/http/routes/admin/AdminAccountRoutesSuite.scala
+++ modules/tests/src/test/scala/tradex/domain/http/routes/admin/AdminAccountRoutesSuite.scala
@@ -19,10 +19,19 @@ import suite.HttpSuite
 import generators._
 import http.routes.TestAccountRepository
 import http.auth.users._
+import cats.data.NonEmptyList
+import tradex.domain.model.account
 
 object AdminAccountRoutesSuite extends HttpSuite {
   def authMiddleware(authUser: AdminUser): AuthMiddleware[IO, AdminUser] =
     AuthMiddleware(Kleisli.pure(authUser))
+
+  final val err = "Unique constraint violation: Attempt to store duplicate account number"
+
+  class TestAccountRepositoryWithFailedStore extends TestAccountRepository {
+    override def store(as: NonEmptyList[account.Account]): IO[Unit] =
+      IO.raiseError(new Exception(err))
+  }
 
   test("POST accounts succeeds") {
     val gen = for {
@@ -35,6 +44,20 @@ object AdminAccountRoutesSuite extends HttpSuite {
       val routes   = AdminAccountRoutes[IO](new TestAccountRepository).routes(authMiddleware(user))
       val expected = JsonObject.singleton("accounts", "Ok".asJson).asJson
       expectHttpBodyAndStatus(routes, req)(expected, Status.Created)
+    }
+  }
+
+  test("POST accounts fails") {
+    val gen = for {
+      cp <- Gen.listOfN(5, tradingCreateAccountGen)
+      au <- adminUserGen
+    } yield (cp, au)
+
+    forall(gen) { case (createAccounts, user) =>
+      val req      = POST(createAccounts, uri"/accounts")
+      val routes   = AdminAccountRoutes[IO](new TestAccountRepositoryWithFailedStore).routes(authMiddleware(user))
+      val expected = err.asJson
+      expectHttpBodyAndStatus(routes, req)(expected, Status.InternalServerError)
     }
   }
 }
