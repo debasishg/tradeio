203793983117e972d96f9a6d117375d5e4738767
Debasish Ghosh <ghosh.debasish@gmail.com>
2021-10-18 18:01:22 +0530

    GenerateTradeRoutesSuite now running ok

--- modules/tests/src/test/scala/tradex/domain/http/routes/secured/GenerateTradeRoutesSuite.scala
+++ modules/tests/src/test/scala/tradex/domain/http/routes/secured/GenerateTradeRoutesSuite.scala
@@ -1,27 +1,58 @@
 package tradex.domain
 package http.routes.secured
 
-import java.time.LocalDate
 import cats.effect._
-import cats.data.{ Kleisli, NonEmptyList }
-import model.account._
-import model.market._
-import model.trade._
-import repository.TradeRepository
+import cats.data.Kleisli
+
+import org.http4s.Method._
+import org.http4s._
+import org.http4s.circe.CirceEntityEncoder._
+import org.http4s.client.dsl.io._
+import org.http4s.server.AuthMiddleware
+import org.http4s.syntax.literals._
+
 import suite.HttpSuite
 import http.auth.users._
-import org.http4s.server.AuthMiddleware
+import generators._
+import services.trading._
+import services.accounting._
+import repository.interpreter.memory._
+import programs.GenerateTrade
 
 object GenerateTradeRoutesSuite extends HttpSuite {
   def authMiddleware(authUser: CommonUser): AuthMiddleware[IO, CommonUser] =
     AuthMiddleware(Kleisli.pure(authUser))
 
-}
+  test("Generate trade succeeds") {
+    val trading = for {
+      a <- AccountRepositoryInterpreter.make[IO]
+      e <- ExecutionRepositoryInterpreter.make[IO]
+      o <- OrderRepositoryInterpreter.make[IO]
+      t <- TradeRepositoryInterpreter.make[IO]
+    } yield Trading.make[IO](a, e, o, t)
 
-protected class TestTradeRepository extends TradeRepository[IO] {
-  def query(accountNo: AccountNo, date: LocalDate): IO[List[Trade]] = IO.pure(List.empty[Trade])
-  def queryByMarket(market: Market): IO[List[Trade]]                = IO.pure(List.empty[Trade])
-  def all: IO[List[Trade]]                                          = IO.pure(List.empty[Trade])
-  def store(trd: Trade): IO[Trade]                                  = ???
-  def store(trades: NonEmptyList[Trade]): IO[Unit]                  = IO.pure(())
+    val accounting = for {
+      b <- BalanceRepositoryInterpreter.make[IO]
+    } yield Accounting.make[IO](b)
+
+    val genTrade = for {
+      t <- trading
+      a <- accounting
+    } yield GenerateTrade(t, a)
+
+    val gen = for {
+      u <- commonUserGen
+      t <- generateTradeFrontOfficeInputGen
+    } yield (u, t)
+
+    forall(gen) { case (user, foTrades) =>
+      val req = POST(foTrades, uri"/generatetrade")
+      val routes = for {
+        t <- trading
+        a <- accounting
+        g <- genTrade
+      } yield GenerateTradeRoutes[IO](g, t, a).routes(authMiddleware(user))
+      routes.flatMap(r => expectHttpStatus(r, req)(Status.Created))
+    }
+  }
 }
