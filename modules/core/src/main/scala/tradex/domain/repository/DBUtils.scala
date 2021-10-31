f03e1d4d49757f86db19112b8a75ee1c1740f34a
Debasish Ghosh <ghosh.debasish@gmail.com>
2021-10-31 13:53:59 +0530

    Refactoring

--- modules/core/src/main/scala/tradex/domain/repository/DBUtils.scala
+++ modules/core/src/main/scala/tradex/domain/repository/DBUtils.scala
@@ -1,7 +1,6 @@
 package tradex.domain
 package repository
 
-import cats.Monad
 import cats.effect._
 import skunk._
 import AccountRepositorySQL._
@@ -9,21 +8,25 @@ import model.account._
 
 object DBUtils {
   case class Paginated[A](as: List[A], more: Boolean)
-  val fetchCount = 16
+  val defaultFetchCount = 16
 
+  // a sample query that fetches based on a specified name pattern (LIKE query in SQL)
+  // this API returns a `Cursor`, which can be traversed through in the user level API
   def queryByNamePattern[F[_]](session: Session[F], namePattern: String): Resource[F, Cursor[F, Account]] =
     session
       .prepare(selectByAccountNamePattern)
-      .flatMap { ps =>
-        ps.cursor(namePattern)
+      .flatMap { pq =>
+        pq.cursor(namePattern)
       }
 
-  def executeQueryWithCursor(session: Session[IO], namePattern: String): IO[Unit] = {
+  // demonstrates usage of a cursor
+  def executeQueryWithCursor(session: Session[IO], namePattern: String, fetchSize: Option[Int] = None): IO[Unit] = {
     def fetchWith(cursor: Cursor[IO, Account]): IO[Unit] = {
-      cursor.fetch(fetchCount).flatMap { case (as, m) =>
+      val fetchCount = fetchSize.getOrElse(defaultFetchCount)
+      cursor.fetch(fetchCount).flatMap { case (as, more) =>
         as.foreach(a => println(a.no))
         println("-" * 20)
-        if (m) fetchWith(cursor)
+        if (more) fetchWith(cursor)
         else IO.unit
       }
     }
