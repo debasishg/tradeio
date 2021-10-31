38650cfb8d2300ed082044e285416d241d0ff0d3
Debasish Ghosh <ghosh.debasish@gmail.com>
2021-10-31 12:53:30 +0530

    Progress with cursor based query

--- modules/core/src/main/scala/tradex/domain/repository/DBUtils.scala
+++ modules/core/src/main/scala/tradex/domain/repository/DBUtils.scala
@@ -2,23 +2,31 @@ package tradex.domain
 package repository
 
 import cats.Monad
+import cats.effect._
 import skunk._
 import AccountRepositorySQL._
 import model.account._
 
 object DBUtils {
   case class Paginated[A](as: List[A], more: Boolean)
-  val fetchCount = 32
+  val fetchCount = 16
 
-  def queryByNamePattern[F[_]: MonadThrowable](session: Session[F], namePattern: String): F[Paginated[Account]] = {
-    val fetchResource =
-      session
-        .prepare(selectByAccountNamePattern)
-        .flatMap { ps =>
-          ps.cursor(namePattern).evalMap(_.fetch(fetchCount))
-        }
-    fetchResource.use { case (accounts, more) =>
-      Monad[F].pure(Paginated(accounts, more))
+  def queryByNamePattern[F[_]](session: Session[F], namePattern: String): Resource[F, Cursor[F, Account]] =
+    session
+      .prepare(selectByAccountNamePattern)
+      .flatMap { ps =>
+        ps.cursor(namePattern)
+      }
+
+  def executeQueryWithCursor(session: Session[IO], namePattern: String): IO[Unit] = {
+    def fetchWith(cursor: Cursor[IO, Account]): IO[Unit] = {
+      cursor.fetch(fetchCount).flatMap { case (as, m) =>
+        as.foreach(a => println(a.no))
+        println("-" * 20)
+        if (m) fetchWith(cursor)
+        else IO.unit
+      }
     }
+    queryByNamePattern(session, namePattern).use(fetchWith)
   }
 }
