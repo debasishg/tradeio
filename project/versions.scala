b7db637890a4def9fed1c5b40f290810f5130116
Scala Steward <me@scala-steward.org>
2022-03-20 01:04:49 +0100

    Update cats-effect to 3.3.8

--- project/versions.scala
+++ project/versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   val catsVersion             = "2.7.0"
-  val catsEffectVersion       = "3.3.4"
+  val catsEffectVersion       = "3.3.8"
   val catsMtlVersion          = "1.2.0"
   val macroParadiseVersion    = "2.1.1"
   val kindProjectorVersion    = "0.13.2"
